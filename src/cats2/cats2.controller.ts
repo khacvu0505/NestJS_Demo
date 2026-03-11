b9f8a83fc72891f71ea0aaa24fbfb9e13d865ad2
Vu Nguyen <vu.nguyenkhac@avironactive.com>
2026-03-11 16:50:09 +0700

    fix: NestJS Fundamentals

--- src/cats2/cats2.controller.ts
+++ src/cats2/cats2.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post, Req } from '@nestjs/common';
 import type { Request } from 'express';
 
 @Controller('cats2')
@@ -6,5 +6,18 @@ export class Cats2Controller {
     @Get()
     findAll(@Req() request: Request): string {
         return 'This action returns all cats2';
+    }
+
+    @Get('sub/*')
+    findSub(@Req() request: Request): string {
+        return 'This action returns a sub route of cats2';
+    }
+
+    @HttpCode(201)
+    @Post()
+    @Header('Cache-Control', 'none')
+    @Header('X-Powered-By', 'NestJS')
+    create(@Req() request: Request): string {
+        return 'This action adds a new cat2';
     }
 }
